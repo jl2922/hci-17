17839c3181b34cac8d580aa93a66a6cdc64b8bf2
Junhao Li <streaver91@gmail.com>
2017-05-24 18:24:53 -0400

    davidson test

--- src/solver/diagonalization/davidson_test.cc
+++ src/solver/diagonalization/davidson_test.cc
@@ -40,29 +40,31 @@ TEST(DavidsonTest, HilbertSystem) {
 
   Davidson davidson(diagonal, apply_hamiltonian, N);
 
-  std::vector<double> initial_vector(N, 0.0);
-  initial_vector[0] = 1.0;
-
-  davidson.diagonalize(initial_vector);
+  const std::vector<double> expectedEigenvalues(
+      {-1.00956719, -0.3518051, -0.23097854, -0.17336724, -0.13218651});
+  const std::vector<std::vector<double>> expectedEigenvectors(
+      {{0.99292536, 0.08026708, 0.04720676, 0.03412438, 0.02694173},
+       {0.10953429, -0.90014126, -0.22310872, -0.14701356, -0.11467862},
+       {0.04208261, 0.42251014, -0.54880665, -0.25894711, -0.19182954},
+       {0.00259482, -0.02195869, -0.78985725, 0.1487066, 0.10266289},
+       {0.01203533, 0.04023094, 0.09953056, -0.90203616, -0.06584302}});
 
   // Check eigenvalue and eigenvector with reference values from exact diagonalization.
-  const std::vector<double> expectedEigenvalues(
-      {1.00956719, 0.3518051, 0.23097854, 0.17336724, 0.13218651});
-  const std::vector<std::vector<double>> expectedEigenvectors(
-      {{0.99292536, 0.10953429, 0.04208261, 0.00259482, 0.01203533},
-       {0.08026708, -0.90014126, 0.42251014, -0.02195869, 0.04023094},
-       {0.04720676, -0.22310872, -0.54880665, -0.78985725, 0.09953056},
-       {0.03412438, -0.14701356, -0.25894711, 0.1487066, -0.90203616},
-       {0.02694173, -0.11467862, -0.19182954, 0.10266289, -0.06584302}});
+  std::vector<double> initial_vector(N, 0.0);
+  initial_vector[0] = 1.0;
+  davidson.diagonalize(initial_vector);
   const double lowest_eigenvalue = davidson.get_lowest_eigenvalue();
-  EXPECT_NEAR(lowest_eigenvalue, -1.00956719, 1.0e-6);
+  EXPECT_NEAR(lowest_eigenvalue, expectedEigenvalues[0], 1.0e-6);
   const std::vector<double> lowest_eigenvector = davidson.get_lowest_eigenvector();
-  const std::vector<double> expected_eigenvector_head(
-      {9.92925358e-01, 8.02670792e-02, 4.72067559e-02});
   for (int i = 0; i < 3; i++) {
-    EXPECT_NEAR(
-        lowest_eigenvector[i],
-        expected_eigenvector_head[i],
-        fabs(expected_eigenvector_head[i] * 0.01));
+    EXPECT_NEAR(lowest_eigenvector[i], expectedEigenvectors[0][i], 1.0e-4);
   }
+
+  // Check
+  std::vector<std::vector<double>> initial_vectors(5);
+  for (int i = 0; i < 5; i++) {
+    initial_vectors[i].assign(N, 0.0);
+    initial_vectors[i][i] = 1.0;
+  }
+  davidson.diagonalize(initial_vectors, 5);
 }
